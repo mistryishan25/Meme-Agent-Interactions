568a5f1ab277c13810177f925d79e6693c0f54f1
Andrew Bassett <abassett907@gmail.com>
2023-11-25 04:45:58 -0500

    Adds networks, lonnberg model, etc

--- utils.py
+++
@@ -1,8 +0,0 @@
-class Logger:
-
-    def __init__(self, verbose=True):
-        self.verbose = verbose
-
-    def __call__(self, msg, *args):
-        if self.verbose:
-            print(msg, *args)
